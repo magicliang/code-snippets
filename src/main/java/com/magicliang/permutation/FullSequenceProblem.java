87d3848c72399c91fa6832d613c0bfc7a6defcc2
吴客 <liangchuan.lc@alibaba-inc.com>
2018-11-11 20:46:56 +0800

    增加使用备忘录的方法来解字符串排列问题。

---
+++ src/main/java/com/magicliang/permutation/FullSequenceProblem.java
@@ -0,0 +1,146 @@
+
+package com.magicliang.permutation;
+
+import com.magicliang.util.MathUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+/**
+ * 全排列问题
+ *
+ * @author magicliang
+ * @version $Id: FullSequenceProblem.java, v 0.1 2018年11月11日 18:55 magicliang Exp $
+ */
+public class FullSequenceProblem {
+
+    public static List<String> getUniqueStrs(List<String> strs) {
+        return strs.stream().distinct().collect(Collectors.toList());
+    }
+
+    /**
+     * 这个算法的时间复杂度大概是O(n 的 n 次方) 超级大的指数时间
+     * @param strs 待字符串列表
+     * @param counter 计数器
+     * @return 组合过的字符串的列表
+     */
+    public static List<String> permutationRecursively(List<String> strs, int[] counter) {
+
+        checkAndIncrease(counter);
+
+        if (null == strs || strs.size() == 0) {
+            return Collections.emptyList();
+        }
+
+        int length = strs.size();
+
+        if (length == 1) {
+            return strs;
+        }
+
+        // 根据乘法原理，子问题的复杂度是全排列数，也就是子问题规模的阶乘
+        int subProblemScale = MathUtil.caculateFactor(length - 1);
+        // 只对长度大于2的字符串求解
+        List<String> result = new ArrayList<>(subProblemScale);
+        for (String basicString : strs) {
+            List<String> others = getOtherStrList(strs, basicString);
+
+            for (String newStr : permutationRecursively(others, counter)) {
+                result.add(basicString + newStr);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<String> dpPermutation(List<String> strs, int[] counter) {
+        Map<String, List<String>> memo = new HashMap<>();
+        return dpPermutationReal(strs, counter, memo);
+    }
+
+    /**
+     * 使用 dp 的备忘录来解这个问题
+     *
+     * @param strs    待字符串列表
+     * @param counter 计数器
+     * @param memo    备忘录
+     * @return 组合过的字符串的列表
+     */
+    private static List<String> dpPermutationReal(List<String> strs, int[] counter, Map<String, List<String>> memo) {
+        checkAndIncrease(counter);
+
+        if (null == strs || strs.size() == 0) {
+            return Collections.emptyList();
+        }
+
+        int length = strs.size();
+
+        if (length == 1) {
+            return strs;
+        }
+
+        // 根据乘法原理，子问题的复杂度是全排列数，也就是子问题规模的阶乘
+        int subProblemScale = MathUtil.caculateFactor(length - 1);
+        // 只对长度大于2的字符串求解
+        List<String> result = new ArrayList<>(subProblemScale);
+        for (String basicString : strs) {
+            List<String> others = getOtherStrList(strs, basicString);
+
+            String sortedKey = generateSortedKey(others);
+
+            List<String> smallResult;
+
+            if (memo.containsKey(sortedKey)) {
+                smallResult = memo.get(sortedKey);
+            } else {
+                smallResult = dpPermutationReal(others, counter, memo);
+                memo.put(sortedKey, smallResult);
+            }
+
+            for (String newStr : smallResult) {
+                result.add(basicString + newStr);
+            }
+        }
+
+        return result;
+
+    }
+
+    private static void checkAndIncrease(int[] counter) {
+        if (null == counter || counter.length != 1) {
+            throw new IllegalArgumentException("There must be only one counter");
+        }
+
+        counter[0] = ++counter[0];
+    }
+
+    private static List<String> getOtherStrList(List<String> strs, String basicString) {
+        List<String> result = new ArrayList<>(strs.size() - 1);
+
+        for (String s : strs) {
+            if (!s.equals(basicString)) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    private static String generateSortedKey(List<String> others) {
+        others.sort(Comparator.naturalOrder());
+        StringJoiner joiner = new StringJoiner(",");
+
+        for (String s : others) {
+            joiner.add(s);
+        }
+
+        return joiner.toString();
+    }
+
+}
---
+++ src/test/java/com/magicliang/permutation/FullSequenceProblemTest.java
@@ -0,0 +1,68 @@
+package com.magicliang.permutation;
+
+import com.magicliang.util.TimeWatchUtil;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 测试全排列问题
+ *
+ * @author liangchuan
+ */
+@Slf4j
+public class FullSequenceProblemTest {
+
+    private static final List<String> STRINGS = new ArrayList<>();
+
+    {
+        //for (int i = 0; i < 15; i++) {
+        //    int characther = 'a' + i;
+        //    STRINGS.add((char)characther + "");
+        //
+        //}
+        STRINGS.add("a");
+        STRINGS.add("b");
+        STRINGS.add("c");
+        STRINGS.add("d");
+        STRINGS.add("f");
+        STRINGS.add("g");
+        STRINGS.add("g");
+    }
+
+    @Test
+    public void getFullSequanceRecursively() {
+        TimeWatchUtil.watch(() -> {
+            int[] counter = {0};
+            List<String> result = FullSequenceProblem.getUniqueStrs(
+                FullSequenceProblem.permutationRecursively(STRINGS, counter));
+            log.info(String.format("一共调用了%d次", counter[0]));
+            log.info(String.format("结果字符串数量一共有：%d", result.size()));
+            log.info(result.toString());
+
+        });
+
+        log.info("递归求解字符串全集合结束");
+    }
+
+    @Test
+    public void dpPermutation() {
+
+        TimeWatchUtil.watch(() -> {
+            int[] counter = {0};
+            List<String> result = FullSequenceProblem.getUniqueStrs(
+                FullSequenceProblem.dpPermutation(STRINGS, counter));
+            log.info(String.format("一共调用了%d次", counter[0]));
+            log.info(String.format("结果字符串数量一共有：%d", result.size()));
+            log.info(result.toString());
+
+        });
+
+        log.info("动态规划求解字符串全集合结束");
+
+    }
+}
