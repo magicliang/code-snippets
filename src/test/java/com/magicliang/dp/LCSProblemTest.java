d380fb1c8eba3b989d6a9ad7100c42a68e7c3e9b
吴客 <liangchuan.lc@alibaba-inc.com>
2018-11-14 18:38:38 +0800

    求解 lcs 问题。

---
+++ src/test/java/com/magicliang/dp/LCSProblemTest.java
@@ -0,0 +1,28 @@
+package com.magicliang.dp;
+
+import com.magicliang.util.MatrixUtil;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ≤‚ ‘ lcs Ω‚∑®
+ *
+ * @author liangchuan
+ */
+@Slf4j
+public class LCSProblemTest {
+
+    private static final String[] SEQUENCE_A = {"A", "B", "C", "B", "D", "A", "B"};
+    private static final String[] SEQUENCE_B = {"B", "D", "C", "A", "B", "A"};
+
+    @Test
+    public void dpLCSProblem() {
+
+        LCSProblem lcsProblem = LCSProblem.dpLCSProblem(SEQUENCE_A, SEQUENCE_B);
+
+        MatrixUtil.printStringMatrix(lcsProblem.getLcsSolutionMatrix());
+        log.info(lcsProblem.getLCSStr());
+    }
+}
